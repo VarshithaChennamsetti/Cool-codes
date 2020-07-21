6d5864414a879eb1cb25c45e44bfba7690c84157
VarshithaChennamsetti <65396462+VarshithaChennamsetti@users.noreply.github.com>
2020-07-21 19:14:43 +0530

    adding the whatsapp_msg_send.py
    For this to work you must be logged into your web.whatsapp.com. This code will open that website one minute before the specified time and send the message at the time mentioned.

---
+++ whatsapp_msg_send.py
@@ -0,0 +1,5 @@
+import pywhatkit
+
+pywhatkit.sendwhatmsg("+91**********","I time travelled to the past to write this message. I think you are the coolest!",19,3)
+
+#For this to work you must be logged into your web.whatsapp.com. This code will open that website one minute before the specified time and send the message at the time mentioned.
